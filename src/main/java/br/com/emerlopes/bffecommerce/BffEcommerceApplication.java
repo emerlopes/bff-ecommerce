577c4a9f1c9cd9528fd8d904a743e418557f5c76
Emerson <lopes.ds@live.com>
2024-07-10 11:24:22 -0300

    chore: criando projeto

---
+++ src/main/java/br/com/emerlopes/bffecommerce/BffEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.emerlopes.bffecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffEcommerceApplication.class, args);
+    }
+
+}
