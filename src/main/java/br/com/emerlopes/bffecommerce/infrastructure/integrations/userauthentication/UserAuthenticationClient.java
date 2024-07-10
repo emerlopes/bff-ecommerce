f2bb4f9f9c333c075b7b154da5035b897e216d63
Emerson <lopes.ds@live.com>
2024-07-10 13:06:38 -0300

    chore: atualizando retorno do método

--- src/main/java/br/com/emerlopes/bffecommerce/infrastructure/integrations/userauthentication/UserAuthenticationClient.java
+++ src/main/java/br/com/emerlopes/bffecommerce/infrastructure/integrations/userauthentication/UserAuthenticationClient.java
@@ -1,28 +1,25 @@
 package br.com.emerlopes.bffecommerce.infrastructure.integrations.userauthentication;
 
-import br.com.emerlopes.bffecommerce.application.shared.response.CustomResponseDTO;
 import br.com.emerlopes.bffecommerce.infrastructure.integrations.userauthentication.request.RegisterUserRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 
 @FeignClient(name = "user-authentication", url = "http://localhost:8080/users")
 public interface UserAuthenticationClient {
 
     @PostMapping("/register-guest")
-    Optional<CustomResponseDTO<CustomerauthenticationResponseDTO>> registerGuestUser(
+    void registerGuestUser(
             @RequestBody RegisterUserRequestDTO registerUserRequestDTO
     );
 
     @PostMapping("/register-user")
-    Optional<CustomResponseDTO<CustomerauthenticationResponseDTO>> registerUser(
+    void registerUser(
             @RequestBody RegisterUserRequestDTO registerUserRequestDTO
     );
 
     @PostMapping("/register-admin")
-    Optional<CustomResponseDTO<CustomerauthenticationResponseDTO>> registerAdminUser(
+    void registerAdminUser(
             @RequestBody RegisterUserRequestDTO registerUserRequestDTO
     );
 
