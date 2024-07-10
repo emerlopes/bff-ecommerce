d00c1f49c0dedcdc478f24cdd5444a575e6d0b53
Emerson <lopes.ds@live.com>
2024-07-10 14:13:30 -0300

    chore: alterando caminho EnableFeignClients

--- src/main/java/br/com/emerlopes/bffecommerce/BffEcommerceApplication.java
+++ src/main/java/br/com/emerlopes/bffecommerce/BffEcommerceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients({"br.com.emerlopes.bffecommerce.infrastructure.*"})
+@EnableFeignClients({"br.com.emerlopes.bffecommerce.infrastructure.integrations*"})
 public class BffEcommerceApplication {
 
     public static void main(String[] args) {
