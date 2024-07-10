c6c7ac334aac835cc66f9f827963f318f413f152
Emerson <lopes.ds@live.com>
2024-07-10 12:49:06 -0300

    chore: configurando FeignClients

--- src/main/java/br/com/emerlopes/bffecommerce/BffEcommerceApplication.java
+++ src/main/java/br/com/emerlopes/bffecommerce/BffEcommerceApplication.java
@@ -2,8 +2,10 @@ package br.com.emerlopes.bffecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients({"br.com.emerlopes.bffecommerce.infrastructure.*"})
 public class BffEcommerceApplication {
 
     public static void main(String[] args) {
