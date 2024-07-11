4e4026e1405c2f021608010670c3e3e6a3e32ac8
Emerson <lopes.ds@live.com>
2024-07-11 15:57:37 -0300

    chore: adicionando JsonProperty

--- src/main/java/br/com/emerlopes/bffecommerce/infrastructure/integrations/msproduct/response/ProductReponseDTO.java
+++ src/main/java/br/com/emerlopes/bffecommerce/infrastructure/integrations/msproduct/response/ProductReponseDTO.java
@@ -1,5 +1,6 @@
 package br.com.emerlopes.bffecommerce.infrastructure.integrations.msproduct.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,8 +8,16 @@ import lombok.Data;
 @Builder
 public class ProductReponseDTO {
     private Long id;
+
+    @JsonProperty("nome")
     private String name;
+
+    @JsonProperty("descricao")
     private String description;
+
+    @JsonProperty("preco")
     private Double price;
+
+    @JsonProperty("quantidade")
     private Integer quantity;
 }
