567cfb6d47ec546f16f55e396a1fff402cb82838
Emerson <lopes.ds@live.com>
2024-07-15 00:58:24 -0300

    doc: adicionando anotacoes

--- src/main/java/br/com/emerlopes/bffecommerce/application/entrypoint/rest/ordershoppingcart/dto/UpdateOrderBffRequestDTO.java
+++ src/main/java/br/com/emerlopes/bffecommerce/application/entrypoint/rest/ordershoppingcart/dto/UpdateOrderBffRequestDTO.java
@@ -1,10 +1,14 @@
 package br.com.emerlopes.bffecommerce.application.entrypoint.rest.ordershoppingcart.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateOrderBffRequestDTO {
     private String status;
 }
